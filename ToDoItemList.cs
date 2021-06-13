eeb37d7fd62ab4dbc7e1280552762d03898c4a66
Josh Kennedy <joshk@digitalmagic.io>
2021-06-12 21:54:17 -0500

    Worked on some of the model for a to do list.

---
+++ ToDoItemList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todolist_dotnet
+{
+    public class ToDoItemList
+    {
+        public List<ToDoItem> items;
+    }
+}
